72ee168b3d78d5bc340a45595f8fc6be0864579a
martin.baillie <martin.baillie@mnetics.co.uk>
2018-04-09 16:39:12 +0100

    Add select-styled component generation example to docs.

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-$ ng new html-sass-reference --prefix hsr --routing --style scss [--dry-run]
-$ ng g m reference -m=app --spec --routing
-$ ng g c reference --flat -m=reference -is -it --spec
-
+ng new html-sass-reference --prefix hsr --routing --style scss --dry-run
+ng g m reference -m=app --spec --routing --dry-run
+ng g c reference --flat -m=reference -is -it --spec --dry-run
+ng g c select-styled --flat -m=reference -is -it
--- src/app/reference/reference.module.ts
+++ src/app/reference/reference.module.ts
@@ -3,13 +3,14 @@ import {CommonModule} from '@angular/common';
 
 import {ReferenceRoutingModule} from './reference-routing.module';
 import {ReferenceComponent} from './reference.component';
+import { SelectStyledComponent } from './elements/select-styled.component';
 
 @NgModule({
     imports: [
         CommonModule,
         ReferenceRoutingModule
     ],
-    declarations: [ReferenceComponent]
+    declarations: [ReferenceComponent, SelectStyledComponent]
 })
 export class ReferenceModule {
 }
