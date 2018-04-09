83ae722050edc864a8c86c8d842dc92b1e7a5fc7
martin.baillie <martin.baillie@mnetics.co.uk>
2018-04-09 16:40:10 +0100

    Add select styled component.

---
+++ src/app/reference/elements/select-styled.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hsr-select-styled',
+  template: `
+    <p>
+      select-styled works!
+    </p>
+  `,
+  styles: []
+})
+export class SelectStyledComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
