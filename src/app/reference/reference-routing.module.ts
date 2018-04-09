fd6331309184bf1e36ae0fefa47d3350271bbfbf
martin.baillie <martin.baillie@mnetics.co.uk>
2018-04-09 16:03:00 +0100

    Add route to reference component.

--- src/app/reference/reference-routing.module.ts
+++ src/app/reference/reference-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ReferenceComponent} from './reference.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'reference', component: ReferenceComponent},
+    {path: '', redirectTo: 'reference', pathMatch: 'full'}, // default route
+    {path: '**', redirectTo: 'reference', pathMatch: 'full'} // any unknown routes
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
