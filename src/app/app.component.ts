d3ae1aacd490bcc899ea5315ffd2c3d5e46ca520
martin.baillie <martin.baillie@mnetics.co.uk>
2018-04-09 15:27:38 +0100

    Change indentation to 4 spaces. Reformat code using Webstorm's ALT-CMD-L.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'hsr-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'hsr-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hsr';
+    title = 'hsr';
 }
