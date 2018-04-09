0428e5299beef1e9677e79d4677374cde2e8b9cd
martin.baillie <martin.baillie@mnetics.co.uk>
2018-04-09 15:56:17 +0100

    Add reference component to reference module.

---
+++ src/app/reference/reference.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hsr-reference',
+  template: `
+    <p>
+      reference works!
+    </p>
+  `,
+  styles: []
+})
+export class ReferenceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/reference/reference.module.ts
+++ src/app/reference/reference.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReferenceRoutingModule } from './reference-routing.module';
+import { ReferenceComponent } from './reference.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ReferenceRoutingModule
   ],
-  declarations: []
+  declarations: [ReferenceComponent]
 })
 export class ReferenceModule { }
