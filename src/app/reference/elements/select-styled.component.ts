2552532bed8a095486c42f8d3f6801d42c357ea4
martin.baillie <martin.baillie@mnetics.co.uk>
2018-04-16 23:52:14 +0100

    Select box has font-awesome down angle.

--- src/app/reference/elements/select-styled.component.ts
+++ src/app/reference/elements/select-styled.component.ts
@@ -3,13 +3,15 @@ import {Component, OnInit} from '@angular/core';
 @Component({
     selector: 'hsr-select-styled',
     template: `
-        <label for="the-id-of-the-selector">Pick a number</label>
-        <div class="to-set-the-background-color">
+        <div class="select-wrapper">
+            <label for="the-id-of-the-selector">Pick a number</label>
             <select id="the-id-of-the-selector">
+                <option>Select your number</option>
                 <option>One</option>
                 <option>Two</option>
                 <option>Three</option>
             </select>
+            <i class="fa fa-angle-down"></i>
         </div>
     `,
     styleUrls: ['./select-styled.component.scss']
